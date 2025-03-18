1fc8daefce6390783e7a7295f7b7d998850619b4
arisss3 <114917205+arisss3@users.noreply.github.com>
2025-03-18 17:19:09 +0100

    2

--- train.py
+++ train.py
@@ -13,20 +13,20 @@ transform = T.Compose([
 
 # root/{class}/x001.jpg
 
-tiny_imagenet_dataset_train = ImageFolder(root='tiny-imagenet-200/train', transform=transform)
-tiny_imagenet_dataset_val = ImageFolder(root='tiny-imagenet-200/val', transform=transform)
+tiny_imagenet_dataset_train = ImageFolder(root='dataset/tiny-imagenet-200/train', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='dataset/tiny-imagenet-200/val', transform=transform)
 
 train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
 val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
 
 val_images_path = 'dataset/tiny-imagenet-200/val/images'
 if os.path.exists(val_images_path):  # Check if the directory exists
-    with open('tiny-imagenet-200/val/val_annotations.txt') as f:
+    with open('dataset/tiny-imagenet-200/val/val_annotations.txt') as f:
         for line in f:
             fn, cls, *_ = line.split('\t')
-            os.makedirs(f'tiny-imagenet-200/val/{cls}', exist_ok=True)
+            os.makedirs(f'dataset/tiny-imagenet-200/val/{cls}', exist_ok=True)
 
-            shutil.copyfile(f'{val_images_path}/{fn}', f'tiny-imagenet-200/val/{cls}/{fn}')
+            shutil.copyfile(f'{val_images_path}/{fn}', f'dataset/tiny-imagenet-200/val/{cls}/{fn}')
 
     shutil.rmtree(val_images_path)  # Remove the directory after processing
 
